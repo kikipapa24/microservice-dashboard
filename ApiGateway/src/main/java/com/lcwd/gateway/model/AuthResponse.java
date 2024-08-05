9cd016b41623eb56059cc3f8b7715166502c18f1
kiran kumari <karishmakiran2012@gmail.com>
2024-08-05 19:12:46 +0530

    Added OKTA implementation code for API gateway and OKTA client for UserService

---
+++ ApiGateway/src/main/java/com/lcwd/gateway/model/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.lcwd.gateway.model;
+
+import lombok.*;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String userId;
+    private  String accessToken;
+    private String refreshToken;
+
+    private long expireAt;
+
+    private Collection<String> authorities;
+
+
+}
